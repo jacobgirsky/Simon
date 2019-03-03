a9ca13203134076f067a1a6b9bade32c1eec6b19
jacobgirsky <jgirsky@my.apsu.edu>
2019-03-03 15:03:54 -0600

    Added about button.

--- app/src/main/java/com/example/simon/MainActivity.java
+++ app/src/main/java/com/example/simon/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.simon;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.about_button).setOnClickListener(new AboutListener());
+
+
+    }
+
+    class AboutListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String message = "<html>" +
+                    "<h2>About the game</h2>" +
+                    "<p>The creators are Bushra and Jacob</p>" +
+                    "<p><b>Song</b> TBD</p>" +
+                    "<p><b>Creator:</b> TBD</p>" +
+                    "<p><b>Link</b></p> " +
+                    "<p><https://opengameart.org/content/caketown-cuteplayful</p>"+
+                    "<p><b>License</b> CC-BY 3.0</p>" +
+                    "</html>"; // need to fix html
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setMessage(Html.fromHtml(message));
+            builder.setPositiveButton("OK" , null);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            TextView tv = dialog.findViewById(android.R.id.message); // sets html in TV
+            tv.setMovementMethod(LinkMovementMethod.getInstance());
+        }
     }
 }
