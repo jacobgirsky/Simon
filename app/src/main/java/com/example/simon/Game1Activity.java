b31c919eb8acb598c65bb24330f4afecfcb39f67
jacobgirsky <jgirsky@my.apsu.edu>
2019-03-04 16:54:21 -0600

    Added game code.

--- app/src/main/java/com/example/simon/Game1Activity.java
+++ app/src/main/java/com/example/simon/Game1Activity.java
@@ -1,23 +1,226 @@
 package com.example.simon;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Game1Activity extends AppCompatActivity {
+
+    ImageButton greenButton, redButton, yellowButton, blueButton;
+    int x;
+    final int MAX_LENGTH = 500;
+    int moves[] = new int[MAX_LENGTH];
+    int currentScore = 0;
+    int highScore = 0;
+    public int numItemsInArray = 0, k = 0, numberOfClicksEachLevel = 0, loseSound, hardness;
+    public SoundPool soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+    Random rand = new Random();
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
 
+        loseSound = soundPool.load(this, R.raw.lose, 1);
+
         findViewById(R.id.how_to_im).setOnClickListener(new AboutListener());
 
+        greenButton = findViewById(R.id.green_im);
+        redButton = findViewById(R.id.red_ib);
+        yellowButton = findViewById(R.id.yellow_ib);
+        blueButton = findViewById(R.id.blue_ib);
 
+        greenButton.setOnTouchListener(clicked);
+        redButton.setOnTouchListener(clicked);
+        yellowButton.setOnTouchListener(clicked);
+        blueButton.setOnTouchListener(clicked);
+
+        playGame();
+
+    }
+
+    View.OnTouchListener clicked = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                switch (v.getId()) {
+                    case R.id.green_im:
+                        x = 1;
+                        break;
+                    case R.id.red_ib:
+                        x = 2;
+                        break;
+                    case R.id.yellow_ib:
+                        x = 4;
+                        break;
+                    case R.id.blue_ib:
+                        x = 3;
+                        break;
+                }
+                if (moves[numberOfClicksEachLevel] != x) { // on wrong click
+                    soundPool.play(loseSound, 1, 1, 1, 0, 1f);
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Game1Activity.this);
+                    alertDialogBuilder.setMessage("GAME OVER, you reached level: " + (numItemsInArray - 1) + ", Do you want to play again?");
+                    alertDialogBuilder.setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    //Start over
+                                    reset();
+                                    playGame();
+                                }
+                            });
+
+                    alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish(); // maybe not!!
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+                    return true;
+                }
+                //on success
+                playSound(v.getId());
+                lightUp(v);
+                numberOfClicksEachLevel++;
+                TextView tv = findViewById(R.id.current_score_tv);
+                TextView textView = findViewById(R.id.high_score_tv);
+
+                if (numItemsInArray == numberOfClicksEachLevel) { //if 4 boxes shown, then activate  function
+                    //playGame only after 4 clicks have been made by the user
+
+                    currentScore++;
+                    tv.setText("Current score: " + currentScore);
+
+                    numberOfClicksEachLevel = 0;
+                    if (numItemsInArray > highScore) {
+                        highScore = numItemsInArray;
+                        textView.setText("High score: " + highScore);
+
+                    }
+                    final Runnable r = new Runnable() {
+                        public void run() {
+                            playGame();
+                        }
+                    };
+                    handler.postDelayed(r, 2000 - 500 * hardness);
+                }
+            }
+            return true;
+        }
+    };
+
+    private void playSound(int id) {
+        //function that play sound according to sound ID
+        int audioRes = 0;
+        switch (id) {
+            case R.id.green_im:
+                audioRes = R.raw.greenbutton;
+                break;
+            case R.id.red_ib:
+                audioRes = R.raw.redbutton;
+                break;
+            case R.id.yellow_ib:
+                audioRes = R.raw.yellowbutton;
+                break;
+            case R.id.blue_ib:
+                audioRes = R.raw.bluebutton;
+                break;
+        }
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, audioRes);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    public void playGame() {
+        addToArray();
+        numItemsInArray++;
+        for (k = 0; k < numItemsInArray; k++) {
+            click(k);
+        }
+    }
+
+    public void click(final int click_index) {
+        //Function that clicks one place randomally on the view
+        final Runnable runnable = new Runnable() {
+            public void run() {
+                if (moves[click_index] == 1) {
+                    playSound(R.id.green_im);
+                    lightUp(greenButton);
+                } else if (moves[click_index] == 2) {
+                    playSound(R.id.red_ib);
+                    lightUp(redButton);
+                } else if (moves[click_index] == 3) {
+                    playSound(R.id.yellow_ib);
+                    lightUp(yellowButton);
+                } else {
+                    playSound(R.id.blue_ib);
+                    lightUp(blueButton);
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, (2000 - 500 * hardness) * click_index);
+    }
+
+
+    private int random() {
+        return rand.nextInt(4) + 1; // generate random number between 1 and 4
+    }
+
+    private void addToArray() {  // add random number to the first free position in the array
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            if (moves[i] == 0) {
+                moves[i] = random();
+                break;
+            }
+        }
+    }
+
+    private void lightUp(View v) {
+        Animation mAnimation = new AlphaAnimation(1, 0);
+        mAnimation.setDuration(300);
+        mAnimation.setInterpolator(new LinearInterpolator());
+        v.startAnimation(mAnimation);
+    }
+
+    private void reset() {//reset the game to initial state
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            moves[i] = 0;
+        }
+        numberOfClicksEachLevel = 0;
+        numItemsInArray = 0;
+        currentScore = 0;
     }
 
     class AboutListener implements View.OnClickListener {
@@ -44,4 +247,5 @@ public class Game1Activity extends AppCompatActivity {
             tv.setMovementMethod(LinkMovementMethod.getInstance());
         }
     }
+
 }
